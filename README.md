cb3b3606e33fb7f31386d37f7ae703956d561471
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-08-22 16:06:13 +0300

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-##
+#Simple-auth-app
--- app/components/LoginFrom.tsx
+++ app/components/LoginFrom.tsx
@@ -7,8 +7,11 @@ const LoginFrom = () => {
     <div className='grid place-items-center h-screen'>
       <div className='shadow-lg rounded-lg p-5 border-t-4 border-black flex justify-start flex-col'>
         <h1 className='font-semibold text-xl border-b-[1px]'>Sign In</h1>
+        {/* Form */}
         <form className='flex flex-col w-full gap-4 my-6'>
+          {/* Error Message */}
         <div className="bg-rose-500 w-full text-white px-2 py-2 rounded-md my-2 text-sm" >Error message</div>
+        {/* Inputs */}
           <div className='flex flex-col justify-start gap-2'>
             <label
               htmlFor='email'
@@ -37,10 +40,11 @@ const LoginFrom = () => {
               className='border-2 px-4 py-2 border-neutral-300 rounded-sm outline-none focus:border-neutral-700 bg-zinc-100/4 shadow-smw-[400px]'
             />
           </div>
-        </form>
         <button className='w-full text-center bg-white border-2 border-black font-semibold px-6 py-2 rounded-md text-black transition hover:bg-black hover:text-white '>
           Login
         </button>
+        </form>
+        {/* To Register Page */}
         <Link href="/register" className="mt-3 text-center">
             Don't have an account? <span className="hover:underline">Register</span>
         </Link>
